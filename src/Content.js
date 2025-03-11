ef76016f45b5b4572192a75db06ddb2a01726e73
Hariharan <haririshi2004@gmail.com>
2025-03-11 17:30:37 +0530

    New Commit

--- src/Content.js
+++ src/Content.js
@@ -1,19 +1,59 @@
 import React from "react"
 import { useState } from "react";
+import { FaRegTrashAlt } from "react-icons/fa";
 
 const Content = () => {
-    const [word, setWord] = useState("Earn");
 
-    function handleNameChange() {
-        const name = ["Grow","Earn","Give"];
-        const int = Math.floor(Math.random()*3);
-        setWord(name[int]);
+    const [items, setItems] = useState([
+        {
+            id: 1,
+            checked: true,
+            item: "Practice Coding"
+        },
+        {
+            id: 2,
+            checked: false,
+            item: "Play Cricket"
+        },
+        {
+            id: 3,
+            checked: true,
+            item: "Read books"
+        },
+    ])
+
+    const handleCheck = (id) => {
+        const listItems = items.map((item) => item.id===id ? {...item, checked:!item.checked} : item);
+        setItems(listItems);
     }
 
+    const handleDelete = (id) => {
+        const listItems = items.filter((item) => item.id!== id)
+        setItems(listItems)
+    }
+    
     return (
         <main>
-            <p>Let {word} money</p>
-            <button onClick={handleNameChange}>Submit</button>
+            {items.length ? (
+                <ul>
+                {items.map((item) => (
+                    <li className="item" key={item.id}>
+                        <input type="checkbox" checked = {item.checked} onClick={() => handleCheck(item.id)}/>
+                        <label 
+                        style={item.checked ? {textDecoration: 'line-through'} : null}>
+                            {item.item}</label>
+                        <FaRegTrashAlt 
+                            role="button"
+                            tabIndex="0"
+                            onClick={() => handleDelete(item.id)}
+                        />
+                    </li>
+                ))}
+            </ul>
+            ) : (
+                <p>List is Empty</p>
+            )}
+            
         </main>
 
     )
